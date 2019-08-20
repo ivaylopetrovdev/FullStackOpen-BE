42b03df0b27a591901fca7e7a72a6e00b37a5c59
Ivaylo Petrov <ivaylop@starsgroup.com>
2019-08-20 15:09:20 +0300

    3.13

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const Person = require('./models/person');
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    },
-];
+// let persons = [
+//     {
+//         "name": "Arto Hellas",
+//         "number": "040-123456",
+//         "id": 1
+//     },
+//     {
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523",
+//         "id": 2
+//     },
+//     {
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345",
+//         "id": 3
+//     },
+//     {
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122",
+//         "id": 4
+//     },
+// ];
 
 morgan.token('dataSent', (req) => {
     return req.dataSent;
@@ -43,7 +45,9 @@ app.use(fetchReqBody);
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :dataSent'));
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons.map(person => person.toJSON()))
+    });
 });
 
 app.post('/api/persons', (request, response) => {
@@ -80,7 +84,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
