d21a3313020b8db0f67689e6fd8bb55f1549ac3e
Ivaylo Petrov <ivaylop@starsgroup.com>
2019-08-21 12:28:39 +0300

    3.17

--- index.js
+++ index.js
@@ -81,6 +81,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson.toJSON())
+        })
+        .catch(error => next(error))
+});
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 };
